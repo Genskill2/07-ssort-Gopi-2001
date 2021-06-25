4d1dedd3d6109c29da80f830fa72a879a834d627
Gopikant kumar <76965991+Gopi-2001@users.noreply.github.com>
2021-06-25 21:18:32 +0530

    Update swap.c

--- swap.c
+++ swap.c
@@ -1,4 +1,4 @@
-void swap_max(int []arr, int end, int start){
+void swap_max(int arr[], int end, int start){
  int max = arr[start];
  int found = start;
  for(int i = start+1;i<end;i++){
@@ -10,7 +10,7 @@ void swap_max(int []arr, int end, int start){
   arr[start] = max;
   arr[found] = temp;
 }
-void ssort(int []arr, int l){
+void ssort(int arr[], int l){
 for(int i = 0;i<l;i++){
 swap_max(arr,l,i);
 }
